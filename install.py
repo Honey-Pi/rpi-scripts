ed6076147f4e197b8913f741d74b96ed654ba267
JavanXD <mail@javan.de>
2018-06-11 10:45:46 +0200

    first drafts

---
+++ install.py
@@ -0,0 +1,37 @@
+﻿# Installiere alle benötigten Packages
+# https://tutorials-raspberrypi.de/raspberry-pi-daten-thingspeak-loggen-auswerten/
+	# Sofern du den Sensor vorher nie benutzt hast und auch die entsprechende Bibliothek noch nicht installiert ist, müssen wir erst einmal diese installieren:
+	sudo apt-get update
+	sudo apt-get install build-essential python-dev python-openssl git
+
+	# Nun installieren wir noch die Bibliothek zum Auslesen des DHT11/22 Sensor
+	git clone https://github.com/adafruit/Adafruit_Python_DHT.git && cd Adafruit_Python_DHT
+	sudo python setup.py install
+
+# https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-bme680-breakout
+	# dieses package hatte bei mir bei raspbian-lite gefehlt:
+	sudo apt-get install python-smbus
+	# You'll need to have I2C enabled:
+	curl https://get.pimoroni.com/i2c | bash
+	
+	# Next, in the terminal, type the following to clone the repository and install it
+	git clone https://github.com/pimoroni/bme680
+	cd bme680/library
+	sudo python setup.py install
+	
+	#To run the example, type the following
+	cd /home/pi/bme680/examples
+	python read-all.py
+	
+# https://tutorials-raspberrypi.de/raspberry-pi-temperatur-mittels-sensor-messen/
+	
+	# Wenn alles entsprechend verkabelt ist, können wir das 1-Wire Protokoll damit aktivieren:
+	sudo modprobe w1-gpio
+	sudo modprobe w1-therm
+	
+# https://tutorials-raspberrypi.de/raspberry-pi-waage-bauen-gewichtssensor-hx711/
+	# dieses package hatte bei mir auf raspbian-lite gefehlt:
+	sudo apt-get install python-numpy
+	sudo apt-get install python-rpi.gpio python3-rpi.gpio
+	
+	git clone https://github.com/tatobari/hx711py
---
+++ read-settings.py
@@ -0,0 +1,29 @@
+# read settings.json which is saved by rpi-webinterface
+import json
+from pprint import pprint
+from pathlib import Path
+
+filename = "./settings/settings.json"
+my_file = Path(filename)
+settings = {}
+
+try:
+    my_abs_path = my_file.resolve()
+except FileNotFoundError:
+    # doesn"t exist => default values
+	settings["simApn"] = "pinternet.interkom.de"
+	settings["simTime"] = 5
+	settings["set_reference_unit"] = 92
+	settings["sensoren"] = [{	"type":0, 
+								"tsField": "Temperatur",
+								"time": 5
+							},{ "type":2, 
+								"tsField": "Gewicht",
+								"time": 5
+							}]
+else:
+    # exists => read values from file
+	with open(filename, encoding="utf-8") as data_file:
+		settings = json.loads(data_file.read())
+
+pprint(settings)
