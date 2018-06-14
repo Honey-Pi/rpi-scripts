26a14ae0e2cfeed4584c3dead62749d0cbcc3c47
Javan Rasokat <Javan.Rasokat@optisoft.de>
2018-06-14 14:44:40 +0200

    changed read-settings to function

---
+++ import-beispiel-fuer-settings.py
@@ -0,0 +1,4 @@
+from pprint import pprint
+from read-settings import getSettings
+
+pprint(getSettings())
--- read-settings.py
+++ read-settings.py
@@ -3,27 +3,30 @@ import json
 from pprint import pprint
 from pathlib import Path
 
-filename = "./settings/settings.json"
-my_file = Path(filename)
-settings = {}
+def getSettings():
+	filename = "./settings/settings.json"
+	my_file = Path(filename)
+	settings = {}
+	
+	try:
+    	my_abs_path = my_file.resolve()
+	except FileNotFoundError:
+	    # doesn"t exist => default values
+		settings["simApn"] = "pinternet.interkom.de"
+		settings["simTime"] = 5
+		settings["set_reference_unit"] = 92
+		settings["sensoren"] = [{	"type":0, 
+									"tsField": "Temperatur",
+									"time": 5
+								},{ "type":2, 
+									"tsField": "Gewicht",
+									"time": 5
+								}]
+	else:
+	    # exists => read values from file
+		with open(filename, encoding="utf-8") as data_file:
+			settings = json.loads(data_file.read())
 
-try:
-    my_abs_path = my_file.resolve()
-except FileNotFoundError:
-    # doesn"t exist => default values
-	settings["simApn"] = "pinternet.interkom.de"
-	settings["simTime"] = 5
-	settings["set_reference_unit"] = 92
-	settings["sensoren"] = [{	"type":0, 
-								"tsField": "Temperatur",
-								"time": 5
-							},{ "type":2, 
-								"tsField": "Gewicht",
-								"time": 5
-							}]
-else:
-    # exists => read values from file
-	with open(filename, encoding="utf-8") as data_file:
-		settings = json.loads(data_file.read())
+	return settings
 
-pprint(settings)
+pprint(getSettings())
