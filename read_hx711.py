ac4a67dcab7e25c7507b87224748dee420d8c5d9
Javan R <mail@javan.de>
2019-02-24 11:07:53 +0100

    improved weight measurement

--- read_hx711.py
+++ read_hx711.py
@@ -4,6 +4,20 @@
 
 from HX711 import HX711
 
+def takeClosest(myList, myNumber):
+    # from list of integers, get number closest to a given value
+    closest = myList[0]
+    for i in range(1, len(myList)):
+        if abs(myList[i] - myNumber) < closest:
+            closest = myList[i]
+    return closest
+
+def average(myList):
+    # Finding the average of a list
+    total = sum(myList)
+    total = float(total)
+    return total / len(myList)
+
 def measure_weight(weight_sensor):
     # weight sensor pins
     pin_dt = 5
@@ -34,14 +48,24 @@ def measure_weight(weight_sensor):
         hx.set_scale_ratio(scale_ratio=reference_unit)
         hx.set_offset(offset=offset)
 
-        weight = hx.get_weight_mean(5) # average from 5 times
+        # improve weight measurement by doing 3 weight measures
+        weightMeasures=[]
+        for i in range(5):
+            weightMeasures.append(hx.get_weight_mean(3))
+
+        # take "best" measure
+        average_weight = int(average(weightMeasures))
+        weight = takeClosest(weightMeasures, average_weight)
+        print("AVG Weight: " + str(average_weight))
+
+        #weight = hx.get_weight_mean(5) # average from 5 times
         if weight is not 0:
             weight = weight/1000  # gramms to kg
         weight = float("{0:.3f}".format(weight)) # float only 3 decimals
 
         # invert weight if flag is set
         if 'invert' in weight_sensor and weight_sensor['invert'] == True:
-        	weight = weight*-1;
+            weight = weight*-1;
 
     except Exception as e:
         print("Reading HX711 failed: " + str(e))
