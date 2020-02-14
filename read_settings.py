a8eba454dcc2de5885a154fc4ce8aa62c9b22d9f
Javan R <mail@javan.de>
2020-02-14 15:57:15 +0100

    improved settings reading. create file with defaults if not exists.

--- read_settings.py
+++ read_settings.py
@@ -2,53 +2,70 @@
 # This file is part of HoneyPi [honey-pi.de] which is released under Creative Commons License Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0).
 # See file LICENSE or go to http://creativecommons.org/licenses/by-nc-sa/3.0/ for full license details.
 
-# read settings.json which is saved by rpi-webinterface
 import io
 import json
 from pathlib import Path
-from utilities import backendFolder
+from utilities import backendFolder, settingsFile
 
+# read settings.json which is created by rpi-webinterface
 def get_settings():
-    filename = backendFolder + '/settings.json'
-    my_file = Path(filename)
     settings = {}
 
     try:
+        my_file = Path(settingsFile)
         my_abs_path = my_file.resolve()
-    except: # FileNotFoundError
-        # doesn"t exist => default values
+    except:
+        # FileNotFoundError: doesn't exist => default values
         settings["button_pin"] = 16
-        settings["interval"] = 300
+        settings["w1gpio"] = 11
+        settings["interval"] = 0
+        settings["debug"] = True
+        settings["offline"] = 0
+        settings["shutdownAfterTransfer"] = False
+        # Write these settings to file
+        write_settings(settings)
 
     else:
-        # exists => read values from file
-        with io.open(filename, encoding="utf-8") as data_file:
+        # File exists => read values from file
+        with io.open(settingsFile, encoding="utf-8") as data_file:
             settings = json.loads(data_file.read())
 
-    settings = check_vars(settings)
+    settings = validate_settings(settings)
     return settings
 
-def check_vars(settings):
-    settingstobewritten = False
+# function to migrate settings and check the setting file
+def validate_settings(settings):
+    updateSettingsFile = False
+
     try:
         settings["button_pin"] = int(settings["button_pin"])
         if not settings["button_pin"]:
             raise Exception("button_pin is not defined.")
     except:
         settings["button_pin"] = 16
+        updateSettingsFile = True
 
     try:
         if not 'debug' in settings:
-            settings["debug"] = 0
+            settings["debug"] = False
     except:
-        settings["debug"] = 0
+        settings["debug"] = False
+        updateSettingsFile = True
 
     try:
         if not 'shutdownAfterTransfer' in settings:
             settings["shutdownAfterTransfer"] = 0
     except:
         settings["shutdownAfterTransfer"] = 0
+        updateSettingsFile = True
 
+    try:
+        settings["offline"]
+    except KeyError:
+        settings["offline"] = 0
+        updateSettingsFile = True
+
+    # Migrate v0.1.1 to v1.0 (Multi-Channel)
     try:
         settings['ts_channels'][0]["ts_channel_id"]
         settings['ts_channels'][0]["ts_write_key"]
@@ -61,32 +78,25 @@ def check_vars(settings):
             ts_channel = {}
             ts_channel['ts_channel_id']= settings["ts_channel_id"]
             ts_channel['ts_write_key'] = settings["ts_write_key"]
-            ts_channels = [] 
+            ts_channels = []
             ts_channels.append(ts_channel)
             settings['ts_channels'] = ts_channels
-            settingstobewritten = True
+            updateSettingsFile = True
         except:
             settings["ts_channels"] = None
 
-    try:
-        settings["offline"]
-    except KeyError:
-        settings["offline"] = 0
-
-    if settingstobewritten:
-        print("Writing updated settings")
+    if updateSettingsFile:
+        print("Info: Settings have been changed because of version migration.")
         write_settings(settings)
+
     return settings
 
 # get sensors by type
 def get_sensors(settings, type):
     try:
         all_sensors = settings["sensors"]
-    except TypeError:
-        # doesn't exist => return empty array
-        return []
-    except KeyError:
-        # doesn't exist => return empty array
+    except:
+        # Key doesn't exist => return empty array
         return []
 
     sensors = [x for x in all_sensors if x["type"] == type]
@@ -97,14 +107,12 @@ def get_sensors(settings, type):
         return sensors
 
 def write_settings(settings):
-    filename = backendFolder + '/settings.json'
-    my_file = Path(filename)
-
     try:
         # write values to file
-        data_file =  open(filename, "w")
+        data_file =  open(settingsFile, "w")
         json.dump(settings, data_file)
+        return True
     except Exception as ex:
         print("write_settings " + str(ex))
 
-    return True    
+    return False
