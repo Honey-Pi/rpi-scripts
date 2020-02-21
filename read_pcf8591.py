95f39cc108f7de374e541c39dc6e7bf4ddc358f0
elschnorro77 <57951003+elschnorro77@users.noreply.github.com>
2020-02-21 14:29:27 +0100

    added function get_raw_voltage(pin)

--- read_pcf8591.py
+++ read_pcf8591.py
@@ -33,3 +33,27 @@ def measure_voltage(ts_sensor):
         print('Error while reading PCF8591 (Voltage): ' + str(e))
 
     return None
+
+def get_raw_voltage(pin):
+    # I2C-address of YL-40 PCF8591
+    address = 0x48
+
+    try:
+        # Create I2C instance and open the bus
+        PCF8591 = smbus.SMBus(get_smbus())
+
+        # Configure PCF8591
+        PCF8591.write_byte(address, 0x40+pin)
+
+        Voltage_8bit = PCF8591.read_byte(address) # = i2cget -y 1 0x48
+        voltage = Voltage_8bit*0.064453125 # convert 8 bit number to voltage 16.5/256 | 16.5V max voltage for 0xff (=3.3V analog output signal)
+
+        if isinstance(voltage, (int, float)):
+            voltage=round(voltage, 4)
+
+        return voltage
+
+    except Exception as e:
+        print("Exeption while getting temperature field: " + str(e))
+
+    return None   
