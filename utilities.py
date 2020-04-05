770642d0a9c288a9e58f81ba56515e56239b186d
elschnorro77 <57951003+elschnorro77@users.noreply.github.com>
2020-04-05 14:23:15 +0200

    New functions get_led_state and toggle_led

--- utilities.py
+++ utilities.py
@@ -24,6 +24,24 @@ def is_zero():
         if 'Zero' in f.read():
             return True
     return False
+    
+def get_led_state(gpio=21):
+   state = GPIO.input(gpio)
+   if state:
+       #LED on
+       print("LED is currently on")
+   else:
+       #LED off'
+       print("LED is currently off")
+   return state
+   
+def toggle_led(gpio, state):
+   if state:
+       #LED on
+       stop_led(gpio)
+   else:
+       #LED off'
+       start_led(gpio)
 
 def stop_led(gpio=21):
     # Turn Raspi-LED off
