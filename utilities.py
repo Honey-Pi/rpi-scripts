42be3ce900c6edcc878191b9539bf24bd6929bc1
elschnorro77 <57951003+elschnorro77@users.noreply.github.com>
2021-01-08 11:43:23 +0100

    Update utilities.py
    Added function to get default gateway

--- utilities.py
+++ utilities.py
@@ -11,11 +11,24 @@ import json
 import RPi.GPIO as GPIO
 from pathlib import Path
 
+import socket, struct
+
 honeypiFolder = '/home/pi/HoneyPi'
 scriptsFolder = honeypiFolder + '/rpi-scripts'
 backendFolder = '/var/www/html/backend'
 settingsFile = backendFolder + '/settings.json'
 wittypi_scheduleFile = backendFolder + "/schedule.wpi"
+
+def get_default_gateway_linux():
+    """Read the default gateway directly from /proc."""
+    with open("/proc/net/route") as fh:
+        for line in fh:
+            fields = line.strip().split()
+            if fields[1] != '00000000' or not int(fields[3], 16) & 2:
+                # If not default route or not RTF_GATEWAY, skip it
+                continue
+
+            return socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
 
 def stop_tv():
     os.system("sudo /usr/bin/tvservice -o")
