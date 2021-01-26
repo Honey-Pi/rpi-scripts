7681ac20549fdf5a5400a18a59a712d9e312d4b3
elschnorro77 <57951003+elschnorro77@users.noreply.github.com>
2021-01-26 19:13:44 +0100

    fix Hostapd
    requires updated version of hostapd.conf.tmpl from HoneyPi/HoneyPi

--- shell-scripts/ap_to_client_mode.sh
+++ shell-scripts/ap_to_client_mode.sh
@@ -2,7 +2,12 @@
 #!/bin/bash
 
 # Stop AP Services
-ifconfig uap0 down
+
+#ifconfig wlan0 down
 systemctl stop hostapd.service
+ifconfig uap0 down
+iw dev uap0 del
+iptables -D POSTROUTING -t nat -j MASQUERADE
+sysctl net.ipv4.ip_forward=0
 systemctl stop dnsmasq.service
-ifconfig wlan0 up
+#ifconfig wlan0 up
--- shell-scripts/client_to_ap_mode.sh
+++ shell-scripts/client_to_ap_mode.sh
@@ -1,10 +1,61 @@
-[ -z $BASH ] && { exec bash "$0" "$@" || exit; }
-#!/bin/bash
+echo '>>> Stopping hostapd.service'
+(systemctl stop hostapd.service || (systemctl unmask hostapd && systemctl stop hostapd))& # if stop fails because service is masked => unmask
 
-ifconfig wlan0 down
+systemctl restart dnsmasq 
+# Fetch current wifi channel
+echo '>>> Fetch current wifi channel'
+CHANNEL=`iwlist wlan0 channel | grep Current | sed 's/.*Channel \([0-9]*\).*/\1/g'`
+# if no network connected
+if [ -z "$CHANNEL" ]; then
+   echo "Info: Currently not connected to a network."
+   CHANNEL="1"
+fi
+# prevent using 5Ghz (uap0: IEEE 802.11 Hardware does not support configured channel)
+#HWMODE=g 2,4GHz
+#HWMODE=a 5 GHz
+if [ "$CHANNEL" -gt "13" ]; then
+   echo "Info: A 5GHz (Channel: $CHANNEL) WiFi is connected."
+   #HWMODE="a"
+   HWMODE="g"
+   CHANNEL="1"
+else
+   echo "Info: Select 2,4GHz (Channel: $CHANNEL) for AccessPoint"
+   HWMODE="g"
+fi
+export CHANNEL && export HWMODE
+# Create the virtual device
+echo '>>> Create the virtual device uap0'
+cat /etc/hostapd/hostapd.conf.tmpl | envsubst > /etc/hostapd/hostapd.conf
+#unblock wlan interface
+
+nmcli radio wifi off
+rfkill unblock wlan
+
+/sbin/iw dev wlan0 interface add uap0 type __ap
+ip link set uap0 up
 ip addr add 192.168.4.1/24 broadcast 192.168.4.255 dev uap0 #dhcpcd not working
-ifconfig uap0 up
-systemctl stop dnsmasq.service
-(systemctl restart hostapd.service || (systemctl unmask hostapd && systemctl start hostapd))& # if restart fails because service is masked => unmask
+
+sleep 1
+echo '>>> Enable routing'
+# Routing
+sysctl net.ipv4.ip_forward=1
+# NAT
+iptables -t nat -A POSTROUTING -j MASQUERADE
 ifconfig wlan0 up
-systemctl start dnsmasq.service
+
+echo '>>> Starting hostapd.service'
+systemctl start hostapd
+#wait for hostapd to crash dhcpcd and restart it
+sleep 5
+echo '>>> Restarting dhcpcd'
+systemctl restart dhcpcd
+#hostapd /etc/hostapd/hostapd.conf
+# Wait for STRG+C
+# Restore NAT
+#iptables -D POSTROUTING -t nat -j MASQUERADE
+# Restore Routing
+#sysctl net.ipv4.ip_forward=0
+# Stop Services
+#systemctl stop dnsmasq 
+#systemctl stop hostapd 
+
