6cbd71c9e465bb88676b117bf46595e112030a84
Javan Rasokat <mail@javan.de>
2021-01-30 16:41:47 +0100

    cleaner and 'ip link' instead of 'ifconfig'

--- shell-scripts/ap_to_client_mode.sh
+++ shell-scripts/ap_to_client_mode.sh
@@ -1,15 +1,13 @@
 [ -z $BASH ] && { exec bash "$0" "$@" || exit; }
 #!/bin/bash
 
-# Stop AP Services
+# Stop AP
 
-#ifconfig wlan0 down
 systemctl stop hostapd.service
 echo '>>> Remove the virtual device uap0'
-ifconfig uap0 down
+ip link set uap0 down
 iw dev uap0 del
 iptables -D POSTROUTING -t nat -j MASQUERADE
 echo '>>> Stop routing'
 sysctl net.ipv4.ip_forward=0
 systemctl stop dnsmasq.service
-#ifconfig wlan0 up
