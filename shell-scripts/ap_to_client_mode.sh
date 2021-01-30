cf317983ac45ec04e3cb09f18740762ecef59f45
Javan Rasokat <mail@javan.de>
2021-01-30 14:01:50 +0100

    clean

--- shell-scripts/ap_to_client_mode.sh
+++ shell-scripts/ap_to_client_mode.sh
@@ -5,9 +5,11 @@
 
 #ifconfig wlan0 down
 systemctl stop hostapd.service
+echo '>>> Remove the virtual device uap0'
 ifconfig uap0 down
 iw dev uap0 del
 iptables -D POSTROUTING -t nat -j MASQUERADE
+echo '>>> Stop routing'
 sysctl net.ipv4.ip_forward=0
 systemctl stop dnsmasq.service
 #ifconfig wlan0 up
