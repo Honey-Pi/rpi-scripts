d48955b56b6d89ed288f867d129b59846b1549ea
elschnorro77 <57951003+elschnorro77@users.noreply.github.com>
2021-02-06 11:23:13 +0100

    Create post-upgrade.sh

---
+++ v1.0.8-beta11/post-upgrade.sh
@@ -0,0 +1,15 @@
+[ -z $BASH ] && { exec bash "$0" "$@" || exit; }
+#!/bin/bash
+# file: update.sh
+#
+# This script will install required software for HoneyPi after an upgrade.
+# It is recommended to run it in your home directory.
+#
+
+# check if sudo is used
+if [ "$(id -u)" != 0 ]; then
+    echo 'Sorry, you need to run this script with sudo'
+    exit 1
+fi
+
+echo '>>> Running post-upgrade script...'
