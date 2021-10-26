c693626200a420518b160158ee64c8a1236d63f0
Javan Rasokat <mail@javan.de>
2021-10-26 12:03:36 +0200

    renamed

--- read_dht_zero.py
+++ read_dht_zero.py
@@ -5,12 +5,14 @@
 
 # This file is deprecated and not used because the Adafruit_DHT does not work with Raspberry Pi OS. (see: https://stackoverflow.com/a/66007330/6696623)
 
+# Because of comapbility issues (as mentioned here: https://github.com/adafruit/Adafruit_CircuitPython_DHT/issues/73 ) this file is still used if you are running HoneyPi on a Raspberry Zero.
+
 import Adafruit_DHT
 import os
 import logging
 os.environ['PYTHON_EGG_CACHE'] = '/usr/local/pylons/python-eggs'
 
-logger = logging.getLogger('HoneyPi.read_dht_old')
+logger = logging.getLogger('HoneyPi.read_dht_zero')
 
 def measure_dht(ts_sensor):
     fields = {}
