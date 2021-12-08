4d59789163c2b96565c495710ca91bee23a6cf29
elschnorro77 <57951003+elschnorro77@users.noreply.github.com>
2021-12-08 19:37:07 +0100

    update LoRaWAN

---
+++ LoRaWAN/otaa_v3.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+"""RAK811 OTAA demo.
+
+Source https://raw.githubusercontent.com/AmedeeBulle/pyrak811/main/examples/otaa_v3.py
+
+Minimalistic OTAA demo (v3.x firmware)
+
+Copyright 2021 Philippe Vanhaesendonck
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
+"""
+import logging
+from random import randint
+from sys import exit
+from time import sleep
+from timeit import default_timer as timer
+from traceback import print_exc
+
+from rak811.rak811_v3 import Rak811
+from ttn_secrets import APP_EUI, APP_KEY
+
+# Set level to logging.DEBUG for a verbose output
+logging.basicConfig(level=logging.INFO)
+
+lora = Rak811()
+
+# Most of the setup should happen only once...
+print('Setup')
+# Ensure we are in LoRaWan mode
+lora.set_config('lora:work_mode:0')
+# Select OTAA
+lora.set_config('lora:join_mode:0')
+# Select region
+lora.set_config('lora:region:EU868')
+# Set keys
+lora.set_config(f'lora:app_eui:{APP_EUI}')
+lora.set_config(f'lora:app_key:{APP_KEY}')
+# Set data rate
+# Note that DR is different from SF and depends on the region
+# See: https://docs.exploratory.engineering/lora/dr_sf/
+# Set Data Rate to 5 which is SF7/125kHz for EU868
+lora.set_config('lora:dr:5')
+
+# Print config
+for line in lora.get_config('lora:status'):
+    print(f'    {line}')
+
+print('Joining')
+start_time = timer()
+lora.join()
+print('Joined in {:.2f} secs'.format(timer() - start_time))
+
+print('Sending packets every minute - Interrupt to cancel loop')
+print('You can send downlinks from the TTN console')
+try:
+    while True:
+        print('Sending packet')
+        # Cayenne lpp random value as analog
+        start_time = timer()
+        lora.send(bytes.fromhex('0102{:04x}'.format(randint(0, 0x7FFF))))
+        print('Packet sent in {:.2f} secs'.format(timer() - start_time))
+
+        while lora.nb_downlinks:
+            print('Downlink received', lora.get_downlink()['data'].hex())
+
+        sleep(60)
+except KeyboardInterrupt:
+    print()
+except Exception:
+    print_exc()
+
+print('Cleaning up')
+lora.close()
+exit(0)
--- lorawan.py
+++ lorawan.py
@@ -4,7 +4,16 @@ import logging
 import struct
 import math
 
+from time import sleep
+from timeit import default_timer as timer
+from traceback import print_exc
+
+from rak811.rak811_v3 import Rak811, Rak811ResponseError, Rak811TimeoutError
+from LoRaWAN.ttn_secrets import APP_EUI, APP_KEY
+
 logger = logging.getLogger('HoneyPi.lorawan')
+
+from random import randint
 
 def convert_lorawan(ts_fields):
     try:
@@ -29,5 +38,100 @@ def convert_lorawan(ts_fields):
             logger.debug(' field: ' + str(field) + ' content: '+ str(ts_fields[field]) + ' Hex: ' + hexstr)
             loraWANstring = loraWANstring + hexstr
         logger.debug('loraWANstring for this channel: '+ loraWANstring)
+        return loraWANstring
     except Exception as ex:
-        logger.exception("Exception in convert_lorawan")
+        logger.exception("Exception in convert_lorawan" + repr(ex))
+
+def init_lorawan():
+    try:
+        lora = Rak811()
+
+        # Most of the setup should happen only once...
+        logger.debug('Setup')
+        # Ensure we are in LoRaWan mode
+        lora.set_config('lora:work_mode:0')
+        # Select OTAA
+        lora.set_config('lora:join_mode:0')
+        # Select region
+        lora.set_config('lora:region:EU868')
+        # Set keys
+        lora.set_config(f'lora:app_eui:{APP_EUI}')
+        lora.set_config(f'lora:app_key:{APP_KEY}')
+        # Set data rate
+        # Note that DR is different from SF and depends on the region
+        # See: https://docs.exploratory.engineering/lora/dr_sf/
+        # Set Data Rate to 5 which is SF7/125kHz for EU868
+        lora.set_config('lora:dr:5')
+
+        # Print config
+        for line in lora.get_config('lora:status'):
+            logger.debug(f'    {line}')
+
+        logger.debug('Joining')
+        start_time = timer()
+        join_lorawan(lora)
+        logger.debug('Joined in {:.2f} secs'.format(timer() - start_time))
+        return lora
+    except Rak811TimeoutError as ex:
+        logger.error(repr(ex))
+    except Rak811ResponseError as ex:
+        logger.error(repr(ex))
+    except Exception as ex:
+        logger.error("Unhandled Exception in init_lorawan" + repr(ex))
+
+def join_lorawan(lora):
+    try:
+        joined = False
+        while not joined:
+            try:
+                lora.join()
+                joined= True
+            except Rak811TimeoutError as ex:
+                logger.error(repr(ex))
+            except Rak811ResponseError as ex:
+                logger.error(repr(ex))
+    except Exception as ex:
+        logger.error("Unhandled Exception in join_lorawan" + repr(ex))
+
+def update_lorawan(lora, port, write_key, ts_fields_cleaned):
+    try:
+        logger.debug('Sending packet')
+        start_time = timer()
+        lorastr=convert_lorawan(ts_fields_cleaned)
+        #logger.debug(str(lorastr))
+        lora.send(lorastr)
+        logger.debug('Packet sent in {:.2f} secs'.format(timer() - start_time))
+
+        while lora.nb_downlinks:
+            logger.debug('Downlink received', lora.get_downlink()['data'].hex())
+    except Rak811TimeoutError as ex:
+        logger.error(repr(ex))
+    except Rak811ResponseError as ex:
+        logger.error(repr(ex))
+    except Exception as ex:
+        logger.error("Unhandled Exception in update_lorawan" + repr(ex))
+
+if __name__ == '__main__':
+    try:
+        logging.basicConfig(level=logging.DEBUG)
+        lora = init_lorawan()
+        
+        # Cayenne lpp random value as analog
+        port = 1
+        write_key="Test"
+        
+        ts_fields = {}
+        ts_fields['field1'] = randint(0, 0x7FFF)
+        ts_fields['field2'] = randint(0, 0x7FFF)
+        #ts_fields = bytes.fromhex('0102{:04x}'.format(randint(0, 0x7FFF)))
+        update_lorawan(lora, port, write_key, ts_fields)
+
+    except (KeyboardInterrupt, SystemExit):
+        logger.error("close_script")
+
+    except Exception as ex:
+        logger.error("Unhandled Exception in __main__ " + repr(ex))
+
+
+
+
