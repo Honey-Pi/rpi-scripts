b23c481381533ad1ea5621ce69b7eaa7d95edd9d
Javan Rasokat <mail@javan.de>
2022-02-09 19:56:20 +0100

    fix dht
    run
    sudo apt-get install python3-psutil

--- read_dht.py
+++ read_dht.py
@@ -6,17 +6,16 @@
 #   sudo pip3 install adafruit-circuitpython-dht
 #   sudo apt-get install libgpiod2
 
+import psutil # for process killing (sudo apt-get install python3-psutil)
 import logging
 logger = logging.getLogger('HoneyPi.read_dht')
 import time
 
 try:
     import adafruit_dht
-
 except ImportError as ex:
-    logger.error("ImportError while importing Adafruit_DHT " + str(ex))
+    logger.error("ImportError while importing adafruit_dht " + str(ex))
 try:
-    #import board
     import digitalio
 except ImportError as ex:
     logger.error("ImportError while importing digitalio " + str(ex))
@@ -24,71 +23,80 @@ except ImportError as ex:
 def measure_dht(ts_sensor):
     fields = {}
     timer = 1
+    max_timer = 8
     errorMessage = ""
-    dht_type= 0
+    dht_type = 22
     pin = 0
     temperature = None
     humidity = None
-    if 'dht_type' in ts_sensor and dht_type is not None:
+    dht = None
+
+    if 'dht_type' in ts_sensor:
         dht_type = int(ts_sensor["dht_type"])
     else:
-        logger.warning("DHT type not defined, using DHT22")
+        logger.warning("DHT type not defined, using DHT22 by default.")
         dht_type = 22
-    if 'pin' in ts_sensor and pin is not None:
+
+    if 'pin' in ts_sensor:
         pin = int(ts_sensor["pin"])
     else:
-        logger.error("DHT PIN not defined!")
+        logger.error("DHT GPIO-Pin not defined!")
         return fields
+
     try:
         SENSOR_PIN = digitalio.Pin(pin)
     except NameError as ex:
-            logger.error("NameError reading DHT " + str(ex))
-            return fields
+        logger.error("NameError reading DHT " + str(ex))
+        return fields
     except Exception as ex:
-            errorMessage = "Setting up DHT PIN failed for GPIO: " + str(pin)
-            logger.error(errorMessage)
-            return fields
-    while timer <= 8:
-        try:
+        errorMessage = "Setting up DHT PIN failed for GPIO: " + str(pin)
+        logger.error(errorMessage)
+        return fields
 
+    for proc in psutil.process_iter():
+        if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
+            proc.kill()
+            logger.debug("Killed process " + proc.name())
+
+    while timer <= max_timer:
+        try:
             # setup sensor
-            logger.debug("Init DHT Sensor..")
             if dht_type == 2302:
                 dht = adafruit_dht.DHT22(SENSOR_PIN, use_pulseio=True)
             elif dht_type == 11:
                 dht = adafruit_dht.DHT11(SENSOR_PIN, use_pulseio=True)
             else:
                 dht = adafruit_dht.DHT22(SENSOR_PIN, use_pulseio=True)
+            temperature = dht.temperature
+            humidity = dht.humidity
+            dht.exit()
+            break # break while if no Exception occured
         except RuntimeError as error:
             # Errors happen fairly often, DHT's are hard to read, just keep going
-            errorMessage = "Failed reading DHT: " + error.args[0]
+            errorMessage = "Failed reading DHT ("+str(timer)+"/"+str(max_timer)+"): " + error.args[0]
             logger.debug(errorMessage)
             time.sleep(1)
             timer = timer + 1
-        except NameError as ex:
-            logger.error("NameError reading DHT " + str(ex))
-            return fields
-        else:
-            try:
-                logger.debug('Measuring temperature (try: '+ str(timer)+')')
-                temperature = dht.temperature
-                logger.debug('Measuring humidity (try: '+ str(timer)+')')
-                humidity = dht.humidity
-                logger.debug('Finished measuring, closing sensor...')
-                dht.exit()
-                logger.debug('Finished closing sensor...')
-                break # break while if no Exception occured
-            except RuntimeError as error:
-                # Errors happen fairly often, DHT's are hard to read, just keep going
-                errorMessage = "Failed reading DHT: " + error.args[0]
-                logger.debug(errorMessage)
-                time.sleep(1)
-                timer = timer + 1
-                dht.exit()
+            pass
+        except:
+            logger.exception("Unhandled Exception in measure_dht")
+            time.sleep(1)
+            timer = timer + 1
+            pass
 
-    if timer > 8: # end reached
-        logger.error("Failed reading DHT (tried "+str(timer)+"x times) on GPIO " + str(pin) + ". " + errorMessage)
+        try:
+            if dht is not None:
+                dht.exit()
+            else:
+                logger.debug("dht is None which means an exception broke the dht initialising.")
+        except:
+            logger.exception("Unhandled Exception in dht.exit()")
+            pass
+
+    if timer >= max_timer: # end reached
+        logger.error("Failed reading DHT ("+str(timer-1)+" times) on GPIO " + str(pin))
         return fields
+
     # Create returned dict if ts-field is defined
     if 'ts_field_temperature' in ts_sensor and temperature is not None:
         if 'offset' in ts_sensor and ts_sensor["offset"] is not None:
@@ -96,15 +104,17 @@ def measure_dht(ts_sensor):
         fields[ts_sensor["ts_field_temperature"]] = round(temperature, 1)
     if 'ts_field_humidity' in ts_sensor and humidity is not None:
         fields[ts_sensor["ts_field_humidity"]] = round(humidity, 1)
+
     return fields
 
 # For testing you can call this script directly (python3 read_dht.py)
 if __name__ == '__main__':
+
     logging.basicConfig(level=logging.DEBUG)
+
     try:
-
-
-        fields = measure_dht ({"dht_type" : 2302, "pin" : 5, 'ts_field_temperature': "temperature", 'ts_field_humidity': "humidity"})
+        #fields = measure_dht ({"dht_type" : 2302, "pin" : 5, 'ts_field_temperature': "temperature", 'ts_field_humidity': "humidity"})
+        fields = measure_dht ({"dht_type" : 22, "pin" : 4, 'ts_field_temperature': "temperature", 'ts_field_humidity': "humidity"})
         if fields != {}:
             print("Temp: {:.1f} F / {:.1f} °C    Humidity: {}% ".format(fields['temperature']* (9 / 5) + 32, fields['temperature'], fields['humidity']))
 
